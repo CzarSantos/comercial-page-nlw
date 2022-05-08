2feebe665fc03a78f0448255fb86efe8c5d1006d
Julio Cesar <czar.santos@outlook.com>
2022-05-08 18:13:08 -0300

    active menu + finalização e ajustes

--- main.js
+++ main.js
@@ -4,7 +4,69 @@ window.addEventListener('scroll', onScroll)
 function onScroll(){
  showNav()
  showBackToTopButton()
+ activteMenu(home)
+ activteMenu(services)
+ activteMenu(about)
+ activteMenu(contact)
 }
+
+
+
+function activteMenu(section){
+  const targetLine = scrollY + innerHeight / 2
+
+  //dados necessssarios
+
+  const sectionTop = section.offsetTop //pega topo 
+  const sectionHeight = section.offsetHeight //pega o tamanho
+
+  // top chegou ou ultrapassssou
+  const sectionTopPassed = targetLine >= sectionTop
+
+
+  //verifica se esta abaixo da linha
+
+  //sseção termina?
+  //soma o top do scrol mais a altura do elemento
+  const sectionEnd = sectionTop + sectionHeight
+
+
+  //finalda seção passou da linha
+  const sectionEndPassed = sectionEnd <= targetLine
+
+  //limites da section
+// ! negando
+  const sectionB = sectionTopPassed && !sectionEndPassed
+
+
+  //pega elemento
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+
+  menuElement.classList.remove('active')
+//se for verdadeiro
+if(sectionB){
+  menuElement.classList.add('active')
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function showNav(){
   const nav = document.querySelector('#navigation')
